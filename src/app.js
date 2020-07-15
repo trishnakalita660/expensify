9011b38e27094e153bb5219d29d39b012ab8c9e1
trishnakalita660 <trishnakalita660@gmail.com>
2020-07-15 23:39:15 +0530

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM, { render } from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss'
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+           this is the boiler plate
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
